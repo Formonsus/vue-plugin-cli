25c50946629afbb805708b33968045c003768698
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2021-04-05 13:16:35 +0200

    feat: depending on the Vue version use different syntax for Vue Router

--- templates/plugin/dev/router/index.ts
+++ templates/plugin/dev/router/index.ts
@@ -1,10 +1,18 @@
-import { createRouter, createWebHistory } from "vue-router";
-
 import routes from "./routes";
+
+<% if (version === 3) { -%>
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 });
+<% } else { -%>
+import VueRouter from "vue-router";
+
+const router = new VueRouter({
+  routes
+});
+<% } -%>
 
 export default router;
