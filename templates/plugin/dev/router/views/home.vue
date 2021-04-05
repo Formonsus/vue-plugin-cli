df53a37858216801dd53883568076aab8e7ee316
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2021-04-05 16:47:37 +0200

    feat: add information paragraph to home page of dev

--- templates/plugin/dev/router/views/home.vue
+++ templates/plugin/dev/router/views/home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="pluginHome">
+    <div class="info">
+      Welcome to your plugin development environment.
+    </div>
     <router-link to="/plugin/counter">Counter</router-link>
   </div>
 </template>
