b60b4711a4a0bf1c3b0fa276a5e425af8cd892c6
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2021-04-05 13:06:08 +0200

    feat: depending on Vue version use different syntax for creating a Vuex store

--- templates/plugin/dev/state/store.ts
+++ templates/plugin/dev/state/store.ts
@@ -1,9 +1,19 @@
-import { createStore } from "vuex";
+<% if (version === 3) { -%>
+  import { createStore } from "vuex";
 
-const store = createStore({
-  state: {},
-  mutations: {},
-  actions: {}
-})
+  const store = createStore({
+    state: {},
+    mutations: {},
+    actions: {}
+  });
+<% } else {
+  import Vuex from "vuex";
+
+  const store = new Vuex.Store({
+    state: {},
+    mutations: {},
+    axtions: {}
+  });
+<% } -%>
 
 export default store;
