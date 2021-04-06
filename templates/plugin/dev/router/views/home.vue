3710a6c192b2a215ff17c9dbd5f34736e6e8105b
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2021-04-06 16:47:52 +0200

    refactor: local development app

--- templates/plugin/dev/router/views/home.vue
+++ templates/plugin/dev/router/views/home.vue
@@ -1,7 +1,25 @@
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    let show = ref(true);
+
+    function hideDeveloperInstructions() {
+      show.value = false;
+    }
+
+    return { show, hideDeveloperInstructions }
+  },
+})
+</script>
+
+
 <template>
   <div class="pluginHome">
-    <div class="info">
+    <div v-if="show" class="developerInstructions">
       Welcome to your plugin development environment.
+      <span @click="hideDeveloperInstructions">X</span>
     </div>
     <router-link to="/plugin/counter">Counter</router-link>
   </div>
