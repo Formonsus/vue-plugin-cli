89d31338f5df0375b78340453486ed76aabfb6fe
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2021-04-06 16:51:38 +0200

    docs: add developer instructions to home page

--- templates/plugin/dev/router/views/home.vue
+++ templates/plugin/dev/router/views/home.vue
@@ -18,7 +18,10 @@ export default defineComponent({
 <template>
   <div class="pluginHome">
     <div v-if="show" class="developerInstructions">
-      Welcome to your plugin development environment.
+      <p>Welcome to your plugin development environment. This is the place where you will be testing your plugin. Please
+      feel free to modify this page, add navigation links, or any other element that will make your development and
+      testing process easier. This development app should represent a real world scenario where your plugin will be used
+      in some other applications.</p>
       <span @click="hideDeveloperInstructions">X</span>
     </div>
     <router-link to="/plugin/counter">Counter</router-link>
