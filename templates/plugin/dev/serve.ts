d70385891cc34f313e4b9e5b1b99f5c6cfd114f8
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2021-04-06 13:57:25 +0200

    docs: add some comment explaining each file purpose

--- templates/plugin/dev/serve.ts
+++ templates/plugin/dev/serve.ts
@@ -1,5 +1,6 @@
+import { createApp } from "vue";
+// Import your plugin from src folder
 import plugin from "../src/entry.esm";
-
 <% if (vuex) { -%>
 import store from "./state/store";
 <% } -%>
@@ -8,24 +9,22 @@ import router from "./router/index";
 <% } -%>
 import Dev from "./serve.vue";
 
-
-import { createApp } from "vue";
-
 const app = createApp(Dev);
-
-<% if (vuex) { -%>
-app.use(store);
-<% } -%>
-
+// Install your plugin from src folder.
+// This is the exact same way you would add this plugin to any other Vue application, after you publish it to for
+// example npm.
 app.use(plugin, {
   router: router,
   store: store,
   routePrefix: "/plugin"
 });
 
+<% if (vuex) { -%>
+app.use(store);
+<% } -%>
+
 <% if (vueRouter) { -%>
 app.use(router);
 <% } -%>
 
 app.mount("#app");
-
--- templates/plugin/dev/serve.vue
+++ templates/plugin/dev/serve.vue
@@ -1,12 +1,9 @@
-<script<% if (ts) { %> lang="ts"<% } %>>
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  name: "ServeDev",
-});
-</script>
-
 <template>
+  <!--
+  You shouldn't have any reason to change this file. Purpose of this file is to serve as main view that is loaded when
+  you run `yarn serve`. Depending on your answers during prompt, it will either use Vure Router or it will directly load
+  home view.
+  -->
   <div id="<%-npmName%>">
     <router-view></router-view>
   </div>
