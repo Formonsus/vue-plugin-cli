2888254e445b5259206f690d90ac110ee7783e6d
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2021-04-15 13:39:34 +0200

    feat: add jest for unit testing

---
+++ templates/plugin/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
