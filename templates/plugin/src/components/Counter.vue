15cbb2f1a8720a9348c838a2db9bc0d0a13ea77f
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2021-04-15 17:10:44 +0200

    chore: add new component

---
+++ templates/plugin/src/components/Counter.vue
@@ -0,0 +1,27 @@
+<script>
+import { defineComponent } from "vue";
+import { mapState, mapActions } from "vuex";
+
+export default defineComponent({
+  name: "Counter",
+  computed: {
+    ...mapState("counterModule", ["counter"])
+  },
+  methods: {
+    ...mapActions({
+      increment: "counterModule/incrementCounter",
+      decrement: "counterModule/decrementCounter"
+    })
+  }
+});
+</script>
+
+<template>
+  <div class="counter">
+    <p>Count: {{ counter }}</p>
+    <p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </p>
+  </div>
+</template>
