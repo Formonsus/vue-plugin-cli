dfa7eee7aaf678b1ebda6a24e0947a93112736f6
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2021-04-16 11:57:57 +0200

    chore: prettier

--- templates/plugin/dev/router/views/home.vue
+++ templates/plugin/dev/router/views/home.vue
@@ -18,19 +18,15 @@ export default defineComponent({
   <div id="pluginHome">
     <div v-if="show" class="developerInstructions">
       <p>
-        Welcome to your plugin development environment. This is the place where
-        you will be testing your plugin. Please feel free to modify this page,
-        add navigation links, or any other element that will make your
-        development and testing process easier. This development app should
-        represent a real world scenario where your plugin will be used in some
-        other applications.
+        Welcome to your plugin development environment. This is the place where you will be testing your plugin. Please
+        feel free to modify this page, add navigation links, or any other element that will make your development and
+        testing process easier. This development app should represent a real world scenario where your plugin will be
+        used in some other applications.
         <br /><br />
-        For example good way to modify this page is to add the navigation links
-        to different pages of your app or to directly display components.
+        For example good way to modify this page is to add the navigation links to different pages of your app or to
+        directly display components.
       </p>
-      <span @click="hideDeveloperInstructions" class="hideDeveloperInstructions"
-        >Hide instructions</span
-      >
+      <span @click="hideDeveloperInstructions" class="hideDeveloperInstructions">Hide instructions</span>
     </div>
     <h1>Welcome to <%-npmName%></h1>
     <router-link to="/plugin/welcome">Plugin Welcome Page</router-link>
